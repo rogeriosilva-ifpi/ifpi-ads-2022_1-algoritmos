0d2fc27deb65ad133fbaac1a8891b935e376b9c3
Rogerio Silva <rogerio410@gmail.com>
2022-03-28 21:00:47 -0300

    Organização Questões. Lista 01 - q1

---
+++ listas_fabio/lista01/q1_velocidade.js
@@ -0,0 +1,15 @@
+import {input, print} from '../../io_utils.js'
+
+function main(){
+    // Entrada
+    const velocidade_ms = Number(input('Velocidade em m/s: '))
+
+    // Processamento
+    const velocidade_kmh = velocidade_ms * 3.6
+
+    // Saída
+    print(`O resultado é ${velocidade_kmh.toFixed(2)} km/h`)
+
+}
+
+main()
