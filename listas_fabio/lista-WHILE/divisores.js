eb7b4ee459112a0c0ef7a80de2230e3c07f3a530
Rogerio Silva <rogerio410@gmail.com>
2022-05-24 10:20:20 -0300

    Questao Divisores FABIO - WHILE

--- listas_fabio/lista-WHILE/divisores.js
+++ listas_fabio/lista-WHILE/divisores.js
@@ -1,1 +1,24 @@
-// TODO
+import prompt from 'prompt-sync'
+const input = prompt()
+
+function main(){
+    let numero = Number(input('Número: '))
+    while (numero  !== 0){
+        mostrar_divisores(numero)
+        numero = Number(input('Número: '))
+    }
+}
+
+function mostrar_divisores(numero){
+    let candidato_a_divisor = numero
+
+    while (candidato_a_divisor > 0){
+        if (numero % candidato_a_divisor === 0){
+            console.log(candidato_a_divisor)
+        }
+
+        candidato_a_divisor--
+    }
+}
+
+main()
